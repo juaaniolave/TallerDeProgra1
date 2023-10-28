b94e3dbe37ca0655eb63cdcb3c04a897b53505ce
artizan3 <129905900+artizan3@users.noreply.github.com>
2023-10-28 20:25:47 -0300

    Nuevos test agencia

--- src/TestAgencia/testAgenciaPersistencia.java
+++ src/TestAgencia/testAgenciaPersistencia.java
@@ -1,12 +1,16 @@
-package testsModeloDatos;
+package TestAgencia;
 
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -39,18 +43,8 @@ public class testAgenciaPersistencia {
 
 	@Before
 	public void setUp() throws Exception {
-		empleador = agencia.registroEmpleador(null, null, null, null, null, limiteInferior);
-		//String claveMapEmpleador="emp1";
-		empleadoPretenso = agencia.registroEmpleado(null, null, null, null, null, limiteInferior);
-		/*String claveMapEmpleado="ep1";
-		HashMap<String, EmpleadoPretenso> mapEmpleados=new HashMap<>();
-		mapEmpleados.put(claveMapEmpleado, empleadoPretenso);
-		HashMap<String, Empleador> mapEmpleadores=new HashMap<>();
-		mapEmpleadores.put(claveMapEmpleador, empleador);
-		agencia.setEmpleadores(mapEmpleadores);
-		agencia.setEmpleados(mapEmpleados);
-		*/
-		
+		this.empleador = (Empleador) agencia.registroEmpleador("user1", "pass1", "nombR1", "123", Constantes.JURIDICA, Constantes.SALUD);
+		this.empleadoPretenso = (EmpleadoPretenso) agencia.registroEmpleado("user2", "pass2", "nombR2", "ap1", "456", 30);
 		contratacion = new Contratacion();
 		comisionesCliente = new HashMap<Cliente, Double>();
 		ArrayList<Contratacion> listacontrataciones = new ArrayList<Contratacion>();
@@ -63,15 +57,37 @@ public class testAgenciaPersistencia {
 		agencia.crearTicketEmpleado(Constantes.PRESENCIAL, 1500, Constantes.JORNADA_COMPLETA, Constantes.JUNIOR, Constantes.EXP_MEDIA, Constantes.TERCIARIOS,empleadoPretenso);
 		agencia.crearTicketEmpleador(Constantes.PRESENCIAL, 1500, Constantes.JORNADA_COMPLETA, Constantes.JUNIOR, Constantes.EXP_MEDIA, Constantes.TERCIARIOS,empleador);
 		
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLimitesRemuneracion() {
+		Assert.assertEquals("el limite superior deberia existir", this.limiteSuperior,3000);
 	}
+	@Test
+	public void testMatch() {
+		agencia.match(empleador,empleadoPretenso);
+	}
+	/*@Test
+	public void testGuardar() {
+		try {
+			agencia.guardarAgencia("archivoagencia");
+			File archivo=new File("archivoagencia");
+			//en ningun lado dice que sea xml la persistencia pero lo es en teoria
+			//asique otra cosa mas para el informe
+			// y por ende no se podria testear cargarAgencia
+			Assert.assertTrue("deberia existir el archivo",archivo.exists());
+		} catch (IOException e) {
+			Assert.fail(e.getMessage());
+		}
+	}*/
+
+	
+
 
 }

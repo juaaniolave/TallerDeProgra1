a732f85b779788c0d6e7a06405944337b9c5ae46
artizan3 <129905900+artizan3@users.noreply.github.com>
2023-10-29 16:25:30 -0300

    algunos metodos mas

--- src/TestAgencia/testAgenciaAlgunosMetodos.java
+++ src/TestAgencia/testAgenciaAlgunosMetodos.java
@@ -3,6 +3,8 @@ package TestAgencia;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 import org.junit.Assert;
@@ -12,10 +14,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import excepciones.ContraException;
+import excepciones.ImposibleModificarTicketsException;
 import excepciones.LimiteInferiorRemuneracionInvalidaException;
 import excepciones.LimiteSuperiorRemuneracionInvalidaException;
 import excepciones.NewRegisterException;
+import excepciones.NombreUsuarioException;
 import modeloDatos.Cliente;
+import modeloDatos.ClientePuntaje;
 import modeloDatos.Contratacion;
 import modeloDatos.EmpleadoPretenso;
 import modeloDatos.Empleador;
@@ -23,13 +29,17 @@ import modeloNegocio.Agencia;
 import util.Constantes;
 
 class testAgenciaAlgunosMetodos {
+	
 	Agencia agencia = Agencia.getInstance();
 	Empleador empleador;
 	EmpleadoPretenso empleadoPretenso;
+	Empleador empleador2;
+	EmpleadoPretenso empleadoPretenso2;
 	Contratacion contratacion;
 	HashMap<Cliente, Double> comisionesCliente;
 	int limiteInferior;
 	int limiteSuperior;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -43,12 +53,16 @@ class testAgenciaAlgunosMetodos {
 		//este es un escenario posible algunos metodos requieren de otros escenarios tambien
 		this.empleador = (Empleador) agencia.registroEmpleador("user1", "pass1", "nombR1", "123", Constantes.JURIDICA, Constantes.SALUD);
 		this.empleadoPretenso = (EmpleadoPretenso) agencia.registroEmpleado("user2", "pass2", "nombR2", "ap1", "456", 30);
-		this.limiteInferior = 2000;
-		this.limiteSuperior = 3000;
-		this.agencia.setLimitesRemuneracion(limiteInferior, limiteSuperior);
+		this.empleador2 = (Empleador) agencia.registroEmpleador("user3", "pass3", "nombR3", "789", Constantes.FISICA, Constantes.COMERCIO_LOCAL);
+		this.empleadoPretenso2 = (EmpleadoPretenso) agencia.registroEmpleado("user4", "pass4", "nombR4", "ap2", "000", 35);
 		this.agencia.crearTicketEmpleado(Constantes.PRESENCIAL, 1500, Constantes.JORNADA_COMPLETA, Constantes.JUNIOR, Constantes.EXP_MEDIA, Constantes.TERCIARIOS,empleadoPretenso);
 		this.agencia.crearTicketEmpleador(Constantes.PRESENCIAL, 1500, Constantes.JORNADA_COMPLETA, Constantes.JUNIOR, Constantes.EXP_MEDIA, Constantes.TERCIARIOS,empleador);
+		this.agencia.crearTicketEmpleado(Constantes.HOME_OFFICE, 2000, Constantes.JORNADA_MEDIA, Constantes.MANAGMENT, Constantes.EXP_NADA, Constantes.SECUNDARIOS,empleadoPretenso2);
+		this.agencia.crearTicketEmpleador(Constantes.INDISTINTO, 4000, Constantes.JORNADA_EXTENDIDA, Constantes.SENIOR, Constantes.EXP_MUCHA, Constantes.PRIMARIOS,empleador2);
 		
+		this.limiteInferior = 1000;
+		this.limiteSuperior = 3000;
+		this.agencia.setLimitesRemuneracion(limiteInferior, limiteSuperior);
 	}
 
 	@AfterEach
@@ -61,6 +75,15 @@ class testAgenciaAlgunosMetodos {
 		agencia.setEmpleadores(vacio1);
 		agencia.setContrataciones(vacio4);
 		agencia.setComisionesUsuarios(vacio3);
+		empleador.setListaDePostulantes(null);
+		empleadoPretenso.setListaDePostulantes(null);
+		empleador2.setListaDePostulantes(null);
+		empleadoPretenso2.setListaDePostulantes(null);
+		empleador.setPuntaje(0);
+		empleadoPretenso.setPuntaje(0);
+		empleador2.setPuntaje(0);
+		empleadoPretenso2.setPuntaje(0);
+		agencia.setLimitesRemuneracion(1000, 3000);
 	}
 
 	@Test
@@ -69,7 +92,7 @@ class testAgenciaAlgunosMetodos {
 	}
 	@Test
 	public void testLimitesRemuneracionInf() {
-		Assert.assertEquals("el limite inferior deberia existir", agencia.getLimiteInferior(),2000);
+		Assert.assertEquals("el limite inferior deberia existir", agencia.getLimiteInferior(),1000);
 	}
 	@Test
 	public void testMatch() {
@@ -85,24 +108,60 @@ class testAgenciaAlgunosMetodos {
 		Contratacion c=new Contratacion(empleador,empleadoPretenso);
 		ArrayList<Contratacion> ac=agencia.getContrataciones();
 		Contratacion c2=ac.get(0);
-		Assert.assertEquals("deberian ser la misma contratacion",c,c2);
-		//Son la misma contratacion pero no tiene implementado el equls por ende dan diferente
+		//ya que las contratafciones no se puede igualar, las desgloso para ver si son la misma
+		//GregorianCalendar calendar = new GregorianCalendar();
+		//Assert.assertEquals("deberian ser los mismos datos de contratacion (FECHA)",calendar,c2.getFecha());
+		//NO TESTEO LA FECHA PORQUE SON IGUALES Y NO DA DE TODAS FORMAS
+		Assert.assertEquals("deberian ser los mismos datos de contratacion (EMPELADO)",c.getEmpleado(),c2.getEmpleado());
+		Assert.assertEquals("deberian ser los mismos datos de contratacion (EMPELADOR)",c.getEmpleador(),c2.getEmpleador());
+		
 		
 	}
 	@Test
 	public void testSetLimitesRemuneracionesc1() {
 		//en este escenario no se chequean las exepciones solo que cargue bien el numero
 		try {
-			agencia.setLimitesRemuneracion(1000, 2000);
-			Assert.assertEquals("el limite debe cambiar",agencia.getLimiteInferior(),1000);
-			Assert.assertEquals("el limite debe cambiar",agencia.getLimiteSuperior(),2000);
+			agencia.setLimitesRemuneracion(500, 1500);
+			Assert.assertEquals("el limite debe cambiar",agencia.getLimiteInferior(),500);
+			Assert.assertEquals("el limite debe cambiar",agencia.getLimiteSuperior(),1500);
 		} catch (LimiteSuperiorRemuneracionInvalidaException e) {
 			Assert.fail(e.getMessage());
 		} catch (LimiteInferiorRemuneracionInvalidaException e) {
 			Assert.fail(e.getMessage());
 		}
+	}
+	@Test
+	public void testGeneraPostulantes() {
+		agencia.generaPostulantes();
+		ClientePuntaje c=new ClientePuntaje(6.0,empleadoPretenso);
+		ClientePuntaje c2=new ClientePuntaje(-3.5,empleadoPretenso2);
+		//probamos la lista de 1 solo empleador porque es suficiente para el ejemplo
+		Assert.assertEquals("el puntaje debe ser el mismo 1",empleador.getListaDePostulantes().get(0).getPuntaje(),c.getPuntaje(),0.0);
+		Assert.assertEquals("el cliente debe ser el mismo 1",empleador.getListaDePostulantes().get(0).getCliente(),c.getCliente());
+		Assert.assertEquals("el puntaje debe ser el mismo 2",empleador.getListaDePostulantes().get(1).getPuntaje(),c2.getPuntaje(),0.0);
+		Assert.assertEquals("el cliente debe ser el mismo 2",empleador.getListaDePostulantes().get(1).getCliente(),c2.getCliente());
+		
+		ClientePuntaje c3=new ClientePuntaje(6.0,empleador);
+		ClientePuntaje c4=new ClientePuntaje(1.0,empleador2);
+		//probamos la lista de 1 solo empleado porque es suficiente para el ejemplo
+		Assert.assertEquals("el puntaje debe ser el mismo 1",empleadoPretenso.getListaDePostulantes().get(0).getPuntaje(),c3.getPuntaje(),0.0);
+		Assert.assertEquals("el cliente debe ser el mismo 1",empleadoPretenso.getListaDePostulantes().get(0).getCliente(),c3.getCliente());
+		Assert.assertEquals("el puntaje debe ser el mismo 2",empleadoPretenso.getListaDePostulantes().get(1).getPuntaje(),c4.getPuntaje(),0.0);
+		Assert.assertEquals("el cliente debe ser el mismo 2",empleadoPretenso.getListaDePostulantes().get(1).getCliente(),c4.getCliente());
 		
 	}
-
+	@Test
+	public void testEliminarTicket() throws ContraException, NombreUsuarioException  {
+			agencia.login("user1", "pass1");
+			agencia.setContrataciones(null);
+			//no entiendo no hay contrataciones y no tira exepcion, PREGUNTAR
+			try {
+				agencia.eliminarTicket();
+				Assert.assertEquals("el ticket deberia ser eliminado",null,empleador.getTicket());
+			} catch (ImposibleModificarTicketsException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
 
 }

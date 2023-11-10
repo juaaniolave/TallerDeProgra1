c7ff6697436ecc708620ce086b89f0669a4ca7a8
artizan3 <129905900+artizan3@users.noreply.github.com>
2023-11-10 15:32:10 -0300

    nuevoProyecto

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controlador"},{"l":"excepciones"},{"l":"main"},{"l":"modeloDatos"},{"l":"modeloNegocio"},{"l":"persistencia"},{"l":"util"},{"l":"vista"}];updateSearchResults();
